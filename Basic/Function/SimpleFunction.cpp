7421d7a41885ca71efa5f82d6624ef55183f5816
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-06-11 23:39:23 +0300

    Create SimpleFunction.cpp

---
+++ Basic/Function/SimpleFunction.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void firstfunction() {
+    int a, b;  
+    cin >> a;
+    cin >> b;  
+    int c = a + b;
+    cout << "The sum is: " << c << endl;  
+}
+
+int main() {
+    firstfunction();  
+    return 0;
+}
+    
