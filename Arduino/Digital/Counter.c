66dc46bcc9ef899a785967c96f271fd8c2620237
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-07-19 12:44:19 +0300

    Create Counter.c

---
+++ Arduino/Digital/Counter.c
@@ -0,0 +1,27 @@
+const int ledPins[4] = {2, 3, 4, 5};
+
+int counter = 0;
+
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(ledPins[i], OUTPUT);
+  }
+}
+
+void loop() {
+  for (int i = 0; i < 4; i++) {
+    if (counter & (1 << i)) {
+      digitalWrite(ledPins[i], HIGH); 
+    } else {
+      digitalWrite(ledPins[i], LOW);  
+    }
+  }
+
+  counter++;
+
+  if (counter > 15) {
+    counter = 0;
+  }
+
+  delay(1000);
+}
