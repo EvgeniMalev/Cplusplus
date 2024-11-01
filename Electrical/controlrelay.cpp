84a410256dce2125f739ec748d5296731f879856
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2024-11-01 11:00:50 +0200

    Create controlrelay.cpp

---
+++ Electrical/controlrelay.cpp
@@ -0,0 +1,25 @@
+const int startButtonPin = 2;
+const int stopButtonPin = 3;
+const int relayPin = 4;
+const unsigned long delayTime = 5000;
+bool isRunning = false;
+
+void setup() {
+  pinMode(startButtonPin, INPUT_PULLUP);
+  pinMode(stopButtonPin, INPUT_PULLUP);
+  pinMode(relayPin, OUTPUT);
+  digitalWrite(relayPin, LOW);
+}
+
+void loop() {
+  if (digitalRead(startButtonPin) == LOW && !isRunning) {
+    isRunning = true;
+    delay(delayTime);
+    digitalWrite(relayPin, HIGH);
+  }
+
+  if (digitalRead(stopButtonPin) == LOW && isRunning) {
+    isRunning = false;
+    digitalWrite(relayPin, LOW);
+  }
+}
