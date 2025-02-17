4a0fd13a7d857cc403d1f75f2b9cca0302782281
Evgeni Malev <126523424+EvgeniMalev@users.noreply.github.com>
2025-02-17 22:57:19 +0200

    Update sortcars.cpp

--- Basic/Argorythms/Sort/sortcars.cpp
+++ Basic/Argorythms/Sort/sortcars.cpp
@@ -1,3 +1,15 @@
-vector<string> cars = {"Fiat", "Alfa Romeo", "Innocanti", "Mazerati", "Lancia", "Ferrari", "Lamborghini"};
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-sort(cars.begin(), cars.end());
+int main() {
+    std::vector<std::string> cars = {"Fiat", "Alfa Romeo", "Innocenti", "Maserati", "Lancia", "Ferrari", "Lamborghini"};
+
+    std::sort(cars.begin(), cars.end());
+
+    for (const auto& car : cars) {
+        std::cout << car << std::endl;
+    }
+
+    return 0;
+}
